0b8ef925ca2bbd90cd9173de3507b8d948afea64
jiaxianyang <jiaxianyang@jd.com>
2023-05-25 18:28:49 +0800

    feature: 自己测试类

---
+++ src/main/java/com/example/design/common/entity/Foo.java
@@ -0,0 +1,13 @@
+package com.example.design.common.entity;
+
+public class Foo {
+    public static void main(String[] args) {
+        boolean flag = true;
+        if (flag) {
+            System.out.println("Hello, Java!");
+        }
+        if (flag == true) {
+            System.out.println("Hello, JVM");
+        }
+    }
+}
---
+++ src/main/java/com/example/design/netty/ChatRoom.java
@@ -0,0 +1,56 @@
+package com.example.design.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class ChatRoom {
+
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new ChatRoomHandler());
+                        }
+                    });
+
+            ChannelFuture future = serverBootstrap.bind(PORT).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    private static class ChatRoomHandler extends ChannelInboundHandlerAdapter {
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            if (msg instanceof String) {
+                // 将接收到的消息广播给所有其他用户
+                ctx.write(msg);
+                ctx.flush();
+            }
+        }
+    }
+
+}
